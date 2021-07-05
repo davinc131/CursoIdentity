15bc0bce228eba0ce0144770b42883a162efebf2
Leonardo Davinc Silva <davinc131@hotmail.com>
2021-07-05 00:46:16 -0300

    Primeiro commit. Uso do Identity com Dapper e EntityFrameworkCore

---
+++ WebApp.Identity/Controllers/HomeController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WebApp.Identity.Models;
+
+namespace WebApp.Identity.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+    private readonly UserManager<MyUser> _userManager;
+
+    public HomeController(ILogger<HomeController> logger, UserManager<MyUser> userManager)
+    {
+      _logger = logger;
+      _userManager = userManager;
+    }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [HttpGet]
+    public ActionResult Login()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginModel model)
+    {
+      if (ModelState.IsValid)
+      {
+        var user = await _userManager.FindByNameAsync(model.UserName);
+        if(user != null)
+        {
+          var b = await _userManager.CheckPasswordAsync(user, model.Password);
+
+          if (b)
+          {
+            var identity = new ClaimsIdentity("cookies");
+            identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.Id));
+            identity.AddClaim(new Claim(ClaimTypes.Name, user.UserName));
+            await HttpContext.SignInAsync("cookies", new ClaimsPrincipal(identity));
+            return RedirectToAction("About");
+          }
+          ModelState.AddModelError("", "Usuário ou senha inválida!");
+        }
+        ModelState.AddModelError("", "Usuário ou senha inválida!");
+      }
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterModel model)
+    {
+      if (ModelState.IsValid)
+      {
+        var user = await _userManager.FindByNameAsync(model.UserName);
+
+        if(user == null)
+        {
+          user = new MyUser()
+          {
+            Id = Guid.NewGuid().ToString(),
+            UserName = model.UserName,
+          };
+          var result = await _userManager.CreateAsync(user, model.Password);
+          Console.WriteLine();
+        }
+
+        return View("Success");
+      }
+      return View();
+    }
+
+    [HttpGet]
+    public ActionResult Register()
+    {
+      return View();
+    }
+
+    [HttpGet]
+    public ActionResult Success()
+    {
+      return View();
+    }
+
+    [HttpGet]
+    [Authorize]
+    public ActionResult About()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
+}
---
+++ WebApp.Identity/Startup.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace WebApp.Identity
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddControllersWithViews();
+
+      var connectionString = @"Integrated Security=SSPI; Persist Security Info=False;User ID=sa;Password=sipef@adm;Initial Catalog=IdentityCurso;Data Source=DESKTOP-6MGSSHL";
+      var migrationAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+      services.AddDbContext<MyUserDbContext>(
+          options => options.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationAssembly) )
+        );
+
+      services.AddIdentityCore<MyUser>(options => {  });
+      services.AddScoped<IUserStore<MyUser>, UserOnlyStore<MyUser, MyUserDbContext>>();
+      services.AddAuthentication("cookies").AddCookie("cookies", options => options.LoginPath = "/Home/Login");
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+      }
+      app.UseStaticFiles();
+
+      app.UseRouting();
+
+      app.UseAuthentication();
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllerRoute(
+                  name: "default",
+                  pattern: "{controller=Home}/{action=Index}/{id?}");
+      });
+    }
+  }
+}
